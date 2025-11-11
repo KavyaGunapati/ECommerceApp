66a1afdc06af49f456bd4570326056f180dfd088
KavyaGunapati <kavya.g@gsstech.in>
2025-11-11 18:48:05 +0530

    Added more

--- Pages/Categories/Delete.cshtml.cs
+++ Pages/Categories/Delete.cshtml.cs
@@ -16,6 +16,19 @@ namespace ECommerceApp.Pages.Categories
         public Category Category { get; set; }
         public void OnGet(int id)
         {
+            Category = _context.Categories.Find(id);
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var category = await _context.Categories.FindAsync(Category.Id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+
         }
     }
 }
---
+++ Pages/Orders/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerceApp.Pages.Orders
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
